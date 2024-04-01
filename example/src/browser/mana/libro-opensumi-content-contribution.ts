ead81ae7a6dd5da08bbea1f407122b907d30cfec
sunshinesmilelk <ximo.lk@antgroup.com>
2024-04-01 16:19:01 +0800

    feat: support read notebook file

--- example/src/browser/mana/libro-opensumi-content-contribution.ts
+++ example/src/browser/mana/libro-opensumi-content-contribution.ts
@@ -1,13 +1,33 @@
 import { ContentContribution } from '@difizen/libro-core';
+import { INotebookContent, NotebookOption } from '@difizen/libro-jupyter';
 import { singleton } from '@difizen/mana-app';
+import { IFileServiceClient } from '@opensumi/ide-file-service';
+import { message } from 'antd';
+import { injector } from '../injector';
 
 @singleton({ contrib: ContentContribution })
 export class LibroOpensumiContentContribution implements ContentContribution {
   canHandle = (options) => {
     return options.loadType === 'libro-opensumi-loader' ? 100 : 1;
   };
-  async loadContent() {
-    const originContent = require('./test.json');
-    return originContent;
+  async loadContent(options: NotebookOption) {
+    const fileServiceClient: IFileServiceClient =
+      injector.get(IFileServiceClient);
+    let notebookContent: INotebookContent;
+    try {
+      const { content } = await fileServiceClient.readFile(
+        options.resource.toString(),
+      );
+      notebookContent = JSON.parse(content.toString());
+    } catch (e) {
+      message.error('File Read Error');
+      notebookContent = {
+        cells: [],
+        metadata: {},
+        nbformat: 4,
+        nbformat_minor: 5,
+      };
+    }
+    return notebookContent;
   }
 }
