0125ac7b20c8a83284d2e71f34ad300f2657b7f5
sunshinesmilelk <ximo.lk@antgroup.com>
2024-04-15 18:05:58 +0800

    feat: support search libro

--- src/browser/libro.command.ts
+++ src/browser/libro.command.ts
@@ -1,4 +1,4 @@
-import { LibroService } from '@difizen/libro-jupyter';
+import { LibroSearchManager, LibroService } from '@difizen/libro-jupyter';
 import { Container } from '@difizen/mana-app';
 import { Autowired } from '@opensumi/di';
 import { EDITOR_COMMANDS } from '@opensumi/ide-core-browser';
@@ -20,6 +20,23 @@ export class LibroCommandContribution implements CommandContribution {
         const libroService = this.manaContainer.get(LibroService);
         const libro = libroService.active;
         libro?.save();
+      },
+      isEnabled: () => {
+        const libroService = this.manaContainer.get(LibroService);
+        if (libroService.focus) {
+          return true;
+        }
+        return false;
+      },
+    });
+    commands.registerHandler(EDITOR_COMMANDS.FOCUS_IF_NOT_ACTIVATE_ELEMENT.id, {
+      execute: () => {
+        const libroService = this.manaContainer.get(LibroService);
+        const libro = libroService.active;
+        const libroSearchManager = this.manaContainer.get(LibroSearchManager);
+        if (libro) {
+          libroSearchManager.showSearchView(libro);
+        }
       },
       isEnabled: () => {
         const libroService = this.manaContainer.get(LibroService);
