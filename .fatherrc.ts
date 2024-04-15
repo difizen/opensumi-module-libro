e45e1d5bd055dc81c71e664904568a71da21ab5f
sunshinesmilelk <ximo.lk@antgroup.com>
2024-04-15 14:34:12 +0800

    chore: use father instead

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,20 @@
-import { defineConfig } from 'father';
-
-export default defineConfig({
-  // more father config: https://github.com/umijs/father/blob/master/docs/config.md
-  esm: { output: 'dist' },
-});
+export default {
+  platform: 'browser',
+  esm: {
+    output: 'es',
+  },
+  cjs: {
+    output: 'lib',
+  },
+  extraBabelPlugins: [
+    ['@babel/plugin-proposal-decorators', { legacy: true }],
+    ['@babel/plugin-transform-flow-strip-types'],
+    ['@babel/plugin-transform-class-properties', { loose: true }],
+    ['@babel/plugin-transform-private-methods', { loose: true }],
+    ['@babel/plugin-transform-private-property-in-object', { loose: true }],
+    ['babel-plugin-parameter-decorator'],
+  ],
+  extraBabelPresets: [
+    ['@babel/preset-typescript', { onlyRemoveTypeImports: true }],
+  ],
+};
