08a6dbbe8dca4038fd95237e8949eae0817a0390
sunshinesmilelk <ximo.lk@antgroup.com>
2024-06-03 19:26:15 +0800

    feat: export kernel panel color tokens

--- src/browser/kernel-panel/index.ts
+++ src/browser/kernel-panel/index.ts
@@ -1,6 +1,7 @@
 import { Injectable, Provider } from '@opensumi/di';
 import { BrowserModule } from '@opensumi/ide-core-browser';
 import { KernelPanelContribution } from './kernel.panel.contribution';
+export * from './kernel.panel.color.tokens';
 
 @Injectable()
 export class KernelPanelModule extends BrowserModule {
